f8711ce068ae50e6948a25311db5443ebdabafda
Paulo Fontes <total12anos@gmail.com>
2025-10-24 13:35:20 -0300

    Feacture-animation
    coloquei animações em toda lading page

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+ScrollReveal().reveal('.logo', {
+    origin: 'left',
+    duration: 2500,
+    distance: '200px', });
+
+ScrollReveal().reveal('.midias', {
+    origin: 'top',
+    duration: 2500,
+    distance: '200px', });
+
+ScrollReveal().reveal('.mini-header', {
+    origin: 'left',
+    duration: 2500,
+    distance: '200px', });
+
+ScrollReveal().reveal('.header-title', {
+    origin: 'left',
+    duration: 2500,
+    distance: '200px', });
+
+ScrollReveal().reveal('.servico-card', {
+    duration: 2500,
+    distance: '200px', });
+
+ScrollReveal().reveal('.experiencia-item', {
+    duration: 3000,
+    distance: '200px', });
+
+ScrollReveal().reveal('.section-title', {
+    duration: 2500,
+    distance: '200px', });
+
+ScrollReveal().reveal('.contato-item', {
+    duration: 2500,
+    distance: '200px', });
+
+ScrollReveal().reveal('#title', {
+    origin: 'left',
+    duration: 2500,
+    distance: '200px', });
+
+ScrollReveal().reveal('#subtitle', {
+    origin: 'left',
+    duration: 2500,
+    distance: '200px', });
+   
+ScrollReveal().reveal('.btn-contato', {
+    origin: 'left',
+    duration: 2500,
+    distance: '200px', });    
+
+ScrollReveal().reveal('.foto-perfil', {
+    origin: 'left',
+    duration: 2500,
+    distance: '200px', });
